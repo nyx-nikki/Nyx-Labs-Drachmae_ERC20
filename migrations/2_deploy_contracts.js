11dcfb5b48288ccfdf43292846ee9a50805ada4a
FELIPE <wafflemakr@gmail.com>
2020-03-03 12:42:51 -0500

    add truffle unit tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,6 @@ const NEW_MINTER = "0x1cAb97Def5fb15a965E875765Fce94073CB980C4";
 module.exports = async function(deployer) {
   // Deploy DAE token
   await deployer.deploy(DAE);
-
   // Add new minter
   const dae = await DAE.deployed();
   await dae.addMinter(NEW_MINTER);
--- test/token.test.js
+++ test/token.test.js
@@ -8,14 +8,25 @@ const NAME = "Drachmae";
 const SYMBOL = "DAE";
 const DECIMALS = 4;
 
-contract("DAE Token", ([owner, user1, user2, random]) => {
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+const NEW_MINTER = "0x1cAb97Def5fb15a965E875765Fce94073CB980C4";
+
+contract("DAE Token", ([owner, user1, user2, user3, random]) => {
   let dae;
+
+  const getLastEvent = async eventName => {
+    const events = await dae.getPastEvents(eventName, {
+      fromBlock: 0,
+      toBlock: "latest"
+    });
+    return events.pop().returnValues;
+  };
 
   beforeEach(async () => {
     dae = await DAE.new();
   });
 
-  describe.only("Token::Deployment", () => {
+  describe("Token::Deployment", () => {
     it("Correct Name", async () => {
       const name = await dae.name();
       assert.equal(name, NAME, "incorrect name");
@@ -41,6 +52,58 @@ contract("DAE Token", ([owner, user1, user2, random]) => {
       let totalSupply = await dae.cap();
       totalSupply = totalSupply.toString() / 10 ** DECIMALS;
       assert.equal(totalSupply, MAX_SUPPLY, "incorrect initial supply");
+    });
+  });
+
+  describe("Token::Minting", () => {
+    it("only address with minter role can mint tokens", async () => {
+      await catchRevert(dae.mint(user1, 5 * 10 ** DECIMALS, { from: random }));
+      await dae.mint(user1, 5 * 10 ** DECIMALS, { from: owner });
+
+      const totalSupply = await dae.totalSupply();
+      assert.equal(totalSupply, 42000050000, "incorrect supply");
+    });
+
+    it("only existing minter can add a new minter", async () => {
+      await catchRevert(dae.addMinter(user2, { from: random }));
+      await dae.addMinter(user2, { from: owner });
+
+      const isMinter = await dae.isMinter(user2);
+      assert(isMinter);
+
+      await dae.mint(user1, 5 * 10 ** DECIMALS, { from: user2 });
+    });
+
+    it("correct transfer event when minting", async () => {
+      await dae.mint(user1, 5 * 10 ** DECIMALS, { from: owner });
+
+      const { from, to, value } = await getLastEvent("Transfer");
+
+      assert.equal(from, ZERO_ADDRESS);
+      assert.equal(to, user1);
+      assert.equal(value, 5 * 10 ** DECIMALS);
+    });
+
+    it("cannot mint more than max supply", async () => {
+      await catchRevert(
+        dae.mint(user1, MAX_SUPPLY * 10 ** DECIMALS, { from: owner })
+      );
+    });
+  });
+
+  describe("Token::Transfer", () => {
+    it("cannot transfer a tokens not owned", async () => {
+      await catchRevert(dae.transfer(user1, 1, { from: random }));
+      await dae.mint(user1, 5 * 10 ** DECIMALS, { from: owner });
+      await dae.transfer(user2, 1, { from: user1 });
+    });
+    it("can delegate DAE transfer", async () => {
+      await dae.mint(user1, 5 * 10 ** DECIMALS, { from: owner });
+      await dae.approve(user2, 1000, { from: user1 });
+
+      await dae.transferFrom(user1, user3, 1000, { from: user2 });
+      const balance = await dae.balanceOf(user3);
+      assert.equal(balance, 1000, "incorrect user3 balance");
     });
   });
 });
